7f087319f7b786abe33103a18963101e390df14c
lovely-imtiaz <107641845+lovely-imtiaz@users.noreply.github.com>
2022-07-11 07:58:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,17 +32,17 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.owner = ['923112345043'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['923112345043'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['923112345043'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'DarK-Ayaz' // Your name ඔබේ නම 
+global.botnma = 'NOBITA BOT INC' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = 'NOBITA-👑🖤' //ownernama,ownername
+global.packname = 'MADE BY' // Sticker package name 
+global.author = 'AYAZ-👑🖤' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
-global.sp = '🔵'
+global.sp = '🍃'
 // Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
 global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
 global.alive =`Hello i am alive now
@@ -51,7 +51,7 @@ Download song type .yt
 Thanks fro using...`
 // Welcome massage  ඔබට කැමති නම් මෙයට ඔබෙ welcome massage  එක යොදන්න 
 global.welcome = `
-🌀 Thanks Fro using sl zim bot
+🌀 Thanks Fro using NOBITA's Bot
 ✾ Some bugs fixing
 😼♥️
 `
@@ -60,12 +60,12 @@ global.sendwelcome = `add`
 // Good bye එක ඔනි නම් remove කියලා දාන්න
 global.sendgoodbye = `remove`
 // Caption 
-global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
+global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 NOBITA's Bot`
 global.mess = {
     success: '✅ Done!',
     admin: 'you must be admin to use this cmd!',
-    botAdmin: 'Bot must be admin neh!',
-    owner: 'This cmd is for Elisa bot owner only',
+    botAdmin: 'Bot must be admin first!',
+    owner: 'This cmd is for NOBITA's bot owner only',
     group: 'oooh sorry this cmd is for groups only!',
     private: 'goossssh it can only used in dms!',
     bot: 'This cmd  its only for the bot host',
